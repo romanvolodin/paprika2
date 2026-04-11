b5b1cac6a8897000111983f765a80b6bad9f3463
Roman Volodin <roman.volodin@gmail.com>
2026-04-12 02:58:39 +0300

    Reformat code

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -55,8 +55,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": (
-            "django.contrib.auth.password_validation."
-            "UserAttributeSimilarityValidator"
+            "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
         ),
     },
     {
